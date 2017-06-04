e43830fea8e48a864cbc15ade75598d3983cbe06
Archer Zhao <changlechangsheng@gmail.com>
2017-06-04 16:45:51 +0800

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -48,5 +48,5 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "products#index"
+  root "welcome#index"
 end
